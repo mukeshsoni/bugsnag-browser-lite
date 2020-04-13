2d4000c451eac42d8b9e6997f66ea784fa8863ec
Mukesh Soni <mukeshsoni@gmail.com>
2020-04-13 11:14:57 +0530

    removed react component for now

--- src/bugsnag.ts
+++ src/bugsnag.ts
@@ -3,8 +3,6 @@ import { BugsnagClient } from "./bugsnag_client";
 export { BugsnagClient } from "./bugsnag_client";
 export { BugsnagErrorReport, NotifiableError } from "./types";
 
-export { ErrorBoundary } from "./bugsnag_react";
-
 export default function bugsnag(apiKey: string) {
   return new BugsnagClient(apiKey);
 }
